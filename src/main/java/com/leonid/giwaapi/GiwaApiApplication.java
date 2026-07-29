445af3c84a53490e6bd0a12dcf0d45e894dd3dcf
leonid <leonid.always@gmail.com>
2026-07-29 14:05:50 +0900

    Initial commit

---
+++ src/main/java/com/leonid/giwaapi/GiwaApiApplication.java
@@ -0,0 +1,13 @@
+package com.leonid.giwaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiwaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GiwaApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/leonid/giwaapi/GiwaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonid.giwaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiwaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
